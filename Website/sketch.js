457058a70c27832a82392e0275ef2b3fe700b3ae
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-04-11 01:13:51 -0300

    Update sketch.js

--- Website/sketch.js
+++ Website/sketch.js
@@ -1,32 +1,31 @@
-//Lesson: Make a lot of variables with your circle
-//That change with time
-var circ = {
-    x: 0,
-    y: 0,
-    diameter: 50
+//Lesson: Create a random painting
+//Use color gradient, different shapes and sizes
+var pt = {
+    x: 100,
+    y: 50
 };
-// var circleX = 0;
-// var circleY = 0;
 var col = {
-    red: 255,
-    green: 255,
-    blue: 255
+    r: 255,
+    g: 0,
+    b: 0
 };
-// var RB = 255;
-// var GB = 255;
-// var BB = 255;
 
 function setup() {
     createCanvas(600, 400);
+    background(0);
+    noStroke();
+}
+function mousePressed(){
+    background(0);
+
 }
 
 function draw() {
-    background(0);
-    fill(col.red, col.green, col.blue);
-    ellipse(circ.x, circ.y, circ.diameter);
-    circ.x = 300 + 250 * sin(millis() / 2000);
-    circ.y = 200 + 150 * sin(2 * millis() / 2000);
-    col.red = 127 + 127 * sin(millis() / 900);
-    col.green = 127 + 127 * sin(millis() / 800);
-    col.blue = 127 + 127 * sin(millis() / 700);
+    pt.x = random(0, width);
+    pt.y = random(0, height);
+    col.b = random(0, 255);
+    col.g = random(0, 255);
+    col.r = random(0, 255);
+    fill(col.r, col.g, col.b);
+    ellipse(pt.x, pt.y, 12);
 }
