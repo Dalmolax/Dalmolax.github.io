1970498749294f4759b4e74d31c9330bb66b7821
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-04-16 01:13:53 -0300

    Update sketch.js

--- Website/sketch.js
+++ Website/sketch.js
@@ -11,15 +11,15 @@ var colm = 255;
 
 function setup() {
     createCanvas(600, 400);
-    xspeed = random(3, 5);
-    yspeed = random(3, 5);
+    xspeed = random(5, 10);
+    yspeed = random(5, 10);
     textSize(16);
 }
 
 function draw() {
     background(0);
-    colx = map(xspeed, -30, 30, 20, 255);
-    coly = map(yspeed, -30, 30, 20, 255);
+    colx = map(xspeed, -20, 20, 20, 255);
+    coly = map(yspeed, -20, 20, 20, 255);
     fill(colm, colx, 0);
     text("\"Hello", width / 2.4, height / 20);
     fill(colm, 0, coly);
@@ -28,18 +28,16 @@ function draw() {
     ellipse(x, y, width / 10);
     x = x + xspeed;
     y = y + yspeed;
-    if (xspeed > 30) {
+    if (xspeed > 20) {
         chnx = random(0.8, 0.9)
     }
     if (xspeed < 10) {
         if (xspeed > 0) {
             chnx = random(1.2, 1.3);
-            colm = random(100, 255);
         }
     }
-    if (yspeed > 30) {
+    if (yspeed > 20) {
         chny = random(0.7, 0.9)
-        colm = random(100, 255);
     }
     if (yspeed < 10) {
         if (yspeed > 0) {
@@ -51,21 +49,25 @@ function draw() {
         x = width - width / 20;
         xspeed = -1 * xspeed;
         xspeed = xspeed * chnx;
+        colm = random(100, 255);
     }
     if (y > height - width / 20) {
         print("Off screen!");
         y = height - width / 20;
         yspeed = -1 * yspeed;
         yspeed = yspeed * chny;
+        colm = random(100, 255);
     }
     if (x < width / 20) {
         x = width / 20;
         xspeed = -1 * xspeed;
         xspeed = xspeed * chnx;
+        colm = random(100, 255);
     }
     if (y < width / 20) {
         y = width / 20;
         yspeed = -1 * yspeed;
         yspeed = yspeed * chny;
+        colm = random(100, 255);
     }
 }
