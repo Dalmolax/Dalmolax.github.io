8c6979efb3d24892b8c32d242d0ade3c918a8d9a
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-08-17 02:05:23 -0300

    Added comets

--- Wallpaper9/sketch.js
+++ Wallpaper9/sketch.js
@@ -1,6 +1,8 @@
 //Create a little solar system
 let planets = [];
 let stars = [];
+let comets = [];
+let animation = [];
 let earth;
 let moon;
 let blueStar;
@@ -8,9 +10,12 @@ let greenStar;
 let star1;
 let star2;
 let star3;
+let cometdata;
+let cometsheet;
 
 function setup() {
     createCanvas(600, 950);
+    createComet();
     createEarth();
     createMoon();
     createStars();
@@ -23,6 +28,10 @@ function draw() {
     }
     for (let i = 0; i < planets.length; i++) {
         planets[i].show();
+    }
+    for (let i = 0; i < comets.length; i++) {
+        comets[i].show();
+        comets[i].animate();
     }
 }
 
@@ -46,7 +55,6 @@ class Planet {
             this.x = width / 2 + this.d * sin(millis() / 4000);
             this.y = height / 2 - this.d * cos(millis() / 4000);
         }
-
         //ellipse(this.x, this.y, this.r);
         image(this.t, this.x - this.r / 2, this.y - this.r / 2, this.r, this.r);
     }
@@ -80,6 +88,36 @@ class Star {
         }
         //ellipse(this.x, this.y, this.r); 
         image(this.im, this.x - this.r / 2, this.y - this.r / 2, this.r, this.r);
+    }
+}
+class Sprite {
+    constructor(animation, x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.animation = animation;
+        this.w = this.animation[0].width;
+        this.len = this.animation.length;
+        this.speed = speed;
+        this.i = 0;
+        this.ii = 900;
+    }
+    show() {
+        let i = floor(this.i) % this.len;
+        image(this.animation[i], this.x, this.y);
+    }
+    animate() {
+        //this.rnd = int(random(0, 30));
+        //this.ii = 0;
+        if (this.ii < this.len * 5) {
+            this.i += this.speed;
+            this.ii += 1;
+        } else {
+            this.rnd = int(random(0, 3000));
+            if (this.rnd == 1) {
+                this.ii = 0;
+            }
+        }
+        //this.i += this.speed;
     }
 }
 
@@ -138,6 +176,18 @@ function createStars() {
     }
 }
 
+function createComet() {
+    let frames = cometdata.frames;
+    for (let i = 0; i < frames.length; i++) {
+        let pos = frames[i].frame;
+        let img = cometsheet.get(pos.x, pos.y, pos.w, pos.h);
+        animation.push(img);
+    }
+    for (let i = 0; i < 5; i++) {
+        comets[i] = new Sprite(animation, random(width), random(height), 0.2);
+    }
+}
+
 function preload() {
     earth = loadImage("littleEarth.png");
     moon = loadImage("littleMoon.png");
@@ -146,4 +196,6 @@ function preload() {
     star3 = loadImage("star3.png");
     blueStar = loadImage("blueStar.png");
     yellowStar = loadImage("yellowStar.png");
+    cometdata = loadJSON("comet.json");
+    cometsheet = loadImage("comet.png");
 }
