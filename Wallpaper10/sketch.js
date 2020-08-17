4dde88e9fd694c3e9c636d4e3c337d1b44feaf6a
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-08-17 16:11:51 -0300

    Added comets, optimized, organized resources

--- Wallpaper10/sketch.js
+++ Wallpaper10/sketch.js
@@ -1,17 +1,22 @@
 //Create a little solar system
 let planets = [];
 let stars = [];
+let comets = [];
+let animation = [];
 let earth;
 let moon;
 let blueStar;
-let yellowStar;
 let greenStar;
 let star1;
 let star2;
 let star3;
+let cometdata;
+let cometsheet;
+let chk = 0;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    createComet();
     createEarth();
     createMoon();
     createStars();
@@ -24,6 +29,10 @@ function draw() {
     }
     for (let i = 0; i < planets.length; i++) {
         planets[i].show();
+    }
+    for (let i = 0; i < comets.length; i++) {
+        comets[i].show();
+        comets[i].animate();
     }
 }
 
@@ -82,6 +91,36 @@ class Star {
         image(this.im, this.x - this.r / 2, this.y - this.r / 2, this.r, this.r);
     }
 }
+class Sprite {
+    constructor(animation, x, y, speed) {
+        this.x = x;
+        this.y = y;
+        this.animation = animation;
+        this.w = this.animation[0].width;
+        this.len = this.animation.length;
+        this.speed = speed;
+        this.i = 0;
+        this.ii = 900;
+    }
+    show() {
+        let i = floor(this.i) % this.len;
+        image(this.animation[i], this.x, this.y);
+    }
+    animate() {
+        //this.rnd = int(random(0, 30));
+        //this.ii = 0;
+        if (this.ii < this.len * 5) {
+            this.i += this.speed;
+            this.ii += 1;
+        } else {
+            this.rnd = int(random(0, 3000));
+            if (this.rnd == 1) {
+                this.ii = 0;
+            }
+        }
+        //this.i += this.speed;
+    }
+}
 
 function createEarth() {
     let x = width / 2;
@@ -106,7 +145,7 @@ function createMoon() {
 }
 
 function createStars() {
-    for (let i = 0; i < 300; i++) {
+    for (let i = 0; i < 150; i++) {
         let x = random(width);
         let y = random(height);
         let r = random(1, 5);
@@ -116,45 +155,52 @@ function createStars() {
         let p = new Star(x, y, r, c, im, ty);
         stars.push(p);
     }
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < random(0, 4); i++) {
         let x = random(width);
         let y = random(height);
-        let r = random(10, 30);
+        let r = random(10, 40);
         let c = 255;
         let im = blueStar;
         let ty = "blue";
         let p = new Star(x, y, r, c, im, ty);
         stars.push(p);
     }
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < random(0, 4); i++) {
         let x = random(width);
         let y = random(height);
-        let r = random(20, 40);
+        let r = random(10, 40);
         let c = 255;
         let im = yellowStar;
         let ty = "yellow";
         let p = new Star(x, y, r, c, im, ty);
         stars.push(p);
     }
-    for (let i = 0; i < 4; i++) {
-        let x = random(width);
-        let y = random(height);
-        let r = random(10, 30);
-        let c = 255;
-        let im = greenStar;
-        let ty = "green";
-        let p = new Star(x, y, r, c, im, ty);
-        stars.push(p);
+}
+
+function createComet() {
+    if (chk == 0) {
+        let frames = cometdata.frames;
+        for (let i = 0; i < frames.length; i++) {
+            let pos = frames[i].frame;
+            let img = cometsheet.get(pos.x, pos.y, pos.w, pos.h);
+            animation.push(img);
+        }
+        chk = 1;
+    }
+    for (let i = 0; i < random(0, 10); i++) {
+        let p = new Sprite(animation, random(width), random(height), 0.2);
+        comets.push(p)
     }
 }
 
 function preload() {
-    earth = loadImage("littleEarth.png");
-    moon = loadImage("littleMoon.png");
-    star1 = loadImage("star1.png");
-    star2 = loadImage("star2.png");
-    star3 = loadImage("star3.png");
-    blueStar = loadImage("blueStar.png");
-    yellowStar = loadImage("yellowStar.png");
-    greenStar = loadImage("greenStar.png");
+    earth = loadImage("resources/littleEarth.png");
+    moon = loadImage("resources/littleMoon.png");
+    star1 = loadImage("resources/star1.png");
+    star2 = loadImage("resources/star2.png");
+    star3 = loadImage("resources/star3.png");
+    blueStar = loadImage("resources/blueStar.png");
+    yellowStar = loadImage("resources/yellowStar.png");
+    cometdata = loadJSON("resources/comet.json");
+    cometsheet = loadImage("resources/comet.png");
 }
