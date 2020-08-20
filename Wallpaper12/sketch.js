a23b3cd31e1f2d8c999f165d35f93215af3d61ec
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-08-20 15:09:16 -0300

    Add Rotational glitch

--- Wallpaper12/sketch.js
+++ Wallpaper12/sketch.js
@@ -1,9 +1,23 @@
-//Template
+let r = 0;
+let vari = 0;
+
 function setup() {
     createCanvas(600, 950);
-    //createCanvas(windowWidth,windowHeight);
+    r = 200;
 }
 
 function draw() {
     background(0);
+    vari = vari + millis() / 10000;
+    translate(width / 2, height / 2);
+    rotate(vari);
+    stroke(255);
+    noFill();
+    strokeWeight(3);
+    //ellipse(width / 2, height / 2, r);
+    ellipse(0, 0, r);
+    curveTightness(-1);
+    //curve(width, height / 2 - r / 2, width / 2, height / 2 - r / 2, width / 2, height / 2 + r / 2, 0, height / 2 + r / 2)
+    curve(width / 2, 0 - r / 2, 0, 0 - r / 2, 0, 0 + r / 2, -width / 2, 0 + r / 2)
+
 }
