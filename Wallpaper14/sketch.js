e28a1931377a8c55f9ef1db5ecd21d3b0892dd42
Dalmolax <63485458+Dalmolax@users.noreply.github.com>
2020-09-02 00:02:14 -0300

    Create Rotating Box

--- Wallpaper14/sketch.js
+++ Wallpaper14/sketch.js
@@ -1,9 +1,15 @@
-//Template
+let angle = 0;
+let frmr = 30;
+
 function setup() {
-    createCanvas(600, 950);
-    //createCanvas(windowWidth,windowHeight);
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    frameRate(30);
 }
 
 function draw() {
     background(0);
+    ortho(-windowWidth, windowWidth, windowHeight, -windowHeight, 0, 1500);
+    rotateX(QUARTER_PI*millis()/1000);
+    rotateY(QUARTER_PI*millis()/1000);
+    box(windowWidth, windowHeight, (windowHeight+windowWidth)/2)
 }
